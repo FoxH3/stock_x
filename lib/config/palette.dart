42e209997a45aa58cb819ef4c18f633a58b97366
LynH3 <lynx_h3@web.de>
2022-11-13 16:44:36 +0100

    Palette: TextTheme eingefügt

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -11,6 +11,30 @@ class Palette with ChangeNotifier {
       surfaceVariant: Color(0xFFF6FDFF), //Unbenutzt
       surfaceTint: Color.fromARGB(230, 199, 195, 195), //Unbenutzt
     ),
+    textTheme: const TextTheme(
+      //Platzhalter für das baldige Stylen der Texte
+      headline5: TextStyle(
+        color: Colors.black,
+      ), // Überschrift (appbar)
+      subtitle1: TextStyle(
+        color: Color.fromARGB(230, 0, 0, 0),
+      ), // Switch Labeltext
+      subtitle2: TextStyle(
+        color: Color(0xFFF6FDFF),
+      ), // Sliding Panel
+      labelMedium: TextStyle(
+        color: Color(0xFFF6FDFF),
+      ), // Sliding Panel
+      bodyText1: TextStyle(
+        color: Color(0xFFF6FDFF),
+      ), // Haupttext2
+      bodyText2: TextStyle(
+        color: Color.fromARGB(230, 0, 0, 0),
+      ),
+      caption: TextStyle(
+        color: Colors.black,
+      ),
+    ),
   );
 
   static final ThemeData lightTheme = ThemeData(
@@ -22,6 +46,30 @@ class Palette with ChangeNotifier {
       tertiary: Color(0xE6333333), // dark=weiß - light=grau
       surfaceVariant: Color(0xFFF6FDFF), //Unbenutzt
       surfaceTint: Color.fromARGB(230, 199, 195, 195), //Unbenutzt
+    ),
+    textTheme: const TextTheme(
+      //Platzhalter für das baldige Stylen der Texte
+      headline5: TextStyle(
+        color: Colors.black,
+      ), // Überschrift (appbar)
+      subtitle1: TextStyle(
+        color: Color.fromARGB(230, 0, 0, 0),
+      ), // Switch Labeltext
+      subtitle2: TextStyle(
+        color: Color(0xFFF6FDFF),
+      ), // Sliding Panel
+      labelMedium: TextStyle(
+        color: Color(0xFFF6FDFF),
+      ), // Sliding Panel
+      bodyText1: TextStyle(
+        color: Color(0xFFF6FDFF),
+      ), // Haupttext2
+      bodyText2: TextStyle(
+        color: Color.fromARGB(230, 0, 0, 0),
+      ),
+      caption: TextStyle(
+        color: Colors.black,
+      ),
     ),
   );
 }

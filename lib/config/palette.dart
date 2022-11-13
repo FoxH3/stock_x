314d5eb37b7376ce3700292fd1694dc702135a0c
LynH3 <lynx_h3@web.de>
2022-11-13 16:32:37 +0100

    Palette Update

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 
 class Palette with ChangeNotifier {
   static final ThemeData darkTheme = ThemeData(
-    colorScheme: const ColorScheme.dark(brightness: Brightness.dark),
+    scaffoldBackgroundColor: const Color(0xE6333333),
+    colorScheme: const ColorScheme.dark(
+      primary: Color(0xFFF6FDFF), // dark=weiß - light=weiß
+      onPrimary: Color(0xE6333333), // dark=grau - light=grau
+      secondary: Color(0xFFF6FDFF), //dark=grau - light=light
+      tertiary: Color(0xFFF6FDFF), //dark=weiß - light=grau
+      surfaceVariant: Color(0xFFF6FDFF), //Unbenutzt
+      surfaceTint: Color.fromARGB(230, 199, 195, 195), //Unbenutzt
+    ),
   );
 
   static final ThemeData lightTheme = ThemeData(
-    colorScheme: const ColorScheme.light(brightness: Brightness.light),
+    scaffoldBackgroundColor: const Color(0xFFF6FDFF),
+    colorScheme: const ColorScheme.light(
+      primary: Color(0xFFF6FDFF), // dark=weiß - light=weiß
+      onPrimary: Color(0xE6333333), // dark=grau - light=grau
+      secondary: Color(0xE6333333), //dark=grau - light=light
+      tertiary: Color(0xE6333333), // dark=weiß - light=grau
+      surfaceVariant: Color(0xFFF6FDFF), //Unbenutzt
+      surfaceTint: Color.fromARGB(230, 199, 195, 195), //Unbenutzt
+    ),
   );
 }

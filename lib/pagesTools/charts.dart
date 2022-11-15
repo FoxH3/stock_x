3b1af6665d2a6d1cb0b19f12e4a4ed658adf3885
SimonOdischo <103313046+SimonOdischo@users.noreply.github.com>
2022-11-15 11:21:50 +0100

    charts Update

---
+++ lib/pagesTools/charts.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: depend_on_referenced_packages
+import 'package:stock_x/models/metall_history_datamodel.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+Widget goldChart(List<GoldHisInfo> liste) {
+  return SizedBox(
+      width: 360,
+      child: Center(
+          child: SfCartesianChart(
+              primaryYAxis: NumericAxis(
+                  numberFormat:
+                      NumberFormat.simpleCurrency(decimalDigits: 0, name: "€"),
+                  edgeLabelPlacement: EdgeLabelPlacement.shift,
+                  title: AxisTitle(text: "Preise in Euro")),
+              primaryXAxis:
+                  NumericAxis(title: AxisTitle(text: "Enwicklung in jahren")),
+              series: <ChartSeries>[
+            FastLineSeries<GoldHisInfo, int>(
+                dataLabelSettings: const DataLabelSettings(isVisible: true),
+                markerSettings: const MarkerSettings(isVisible: true),
+                color: const Color.fromARGB(255, 255, 200, 0),
+                dataSource: liste,
+                xValueMapper: (GoldHisInfo data, _) => data.year,
+                yValueMapper: (GoldHisInfo data, _) => data.price)
+          ])));
+}

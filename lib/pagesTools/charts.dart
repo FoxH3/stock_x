6fd3cc436a22bdb24c840fdc6f51e3bbb32bf6dd
SimonOdischo <103313046+SimonOdischo@users.noreply.github.com>
2022-11-15 11:23:00 +0100

    charts update

--- lib/pagesTools/charts.dart
+++ lib/pagesTools/charts.dart
@@ -26,3 +26,44 @@ Widget goldChart(List<GoldHisInfo> liste) {
                 yValueMapper: (GoldHisInfo data, _) => data.price)
           ])));
 }
+
+Widget silberChart(List<SilverHisInfo> liste) {
+  return SizedBox(
+      width: 360,
+      child: Center(
+          child: SfCartesianChart(
+              primaryYAxis: NumericAxis(
+                  numberFormat:
+                      NumberFormat.simpleCurrency(decimalDigits: 0, name: "€"),
+                  edgeLabelPlacement: EdgeLabelPlacement.shift,
+                  title: AxisTitle(text: "Preise in Euro")),
+              primaryXAxis:
+                  NumericAxis(title: AxisTitle(text: "Enwicklung in jahren")),
+              series: <ChartSeries>[
+            FastLineSeries<SilverHisInfo, int>(
+                color: const Color.fromARGB(255, 158, 158, 158),
+                dataLabelSettings: const DataLabelSettings(isVisible: true),
+                markerSettings: const MarkerSettings(isVisible: true),
+                dataSource: liste,
+                xValueMapper: (SilverHisInfo data, _) => data.year,
+                yValueMapper: (SilverHisInfo data, _) => data.price)
+          ])));
+}
+
+//  Future<List<GoldHisInfo>> loadChartData()async{
+//   List<int> years = [];
+//   years = await getYears("jahren für die App", jahren);
+//   List<GoldHisInfo> liste = [];
+//      var docSnapshot2 = await FirebaseFirestore.instance
+//       .collection('stockData')
+//       .doc("Gold History")
+//       .get();
+//   if (docSnapshot2.exists) {
+//     Map<String, dynamic>? data = docSnapshot2.data();
+//     liste.add(GoldHisInfo(price: data?["1.preis"], year: years[0]));
+//     liste.add(GoldHisInfo(price: data?["2.preis"], year: years[1]));
+//     liste.add(GoldHisInfo(price: data?["3.preis"], year: years[2]));
+//     liste.add(GoldHisInfo(price: data?["4.preis"], year: years[3]));
+//   }
+//   return liste;
+// }

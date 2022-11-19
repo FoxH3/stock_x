6ee2444999a456e00a155d5070b28bf1f87fca4f
SimonOdischo <103313046+SimonOdischo@users.noreply.github.com>
2022-11-19 11:36:21 +0100

    pages tools update

--- lib/pagesTools/tools.dart
+++ lib/pagesTools/tools.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stock_x/animation/refresh_animation.dart';
 
 AppBar appBar(String text) {
   return AppBar(
@@ -16,4 +17,29 @@ AppBar appBar(String text) {
       style: const TextStyle(color: Colors.black),
     ),
   );
+}
+
+Widget loading() {
+  return Scaffold(
+      body: Center(
+          child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      WidgetCircularAnimator(
+        child: Container(
+          decoration:
+              BoxDecoration(shape: BoxShape.circle, color: Colors.grey[200]),
+          child: const Icon(
+            Icons.refresh_rounded,
+            color: Colors.red,
+            size: 60,
+          ),
+        ),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      const Text("Daten werden geladen"),
+    ],
+  )));
 }

65275cc5a8eaef0b72b1a2248d6e3fd874ccc209
SimonOdischo <103313046+SimonOdischo@users.noreply.github.com>
2023-01-07 15:13:41 +0100

    dataModel update

--- lib/models/metall_history_datamodel.dart
+++ lib/models/metall_history_datamodel.dart
@@ -1,99 +1,48 @@
-// ignore_for_file: non_constant_identifier_names
-
+/*
+modelClass für Gold und Silber historie informationen
+*/
 class GoldHisInfo {
-  String metal, currency;
-  double price,
-      price_gram_24k,
-      price_gram_22k,
-      price_gram_21k,
-      price_gram_20k,
-      price_gram_18k;
+  double price;
   int year;
-  GoldHisInfo(
-      {required this.year,
-      required this.metal,
-      required this.currency,
-      required this.price,
-      required this.price_gram_24k,
-      required this.price_gram_22k,
-      required this.price_gram_21k,
-      required this.price_gram_20k,
-      required this.price_gram_18k});
+  GoldHisInfo({
+    required this.year,
+    required this.price,
+  });
 
   factory GoldHisInfo.fromJson(Map<String, dynamic> infoJson) {
     return GoldHisInfo(
       year: infoJson['year'] ?? "",
-      metal: infoJson['metal'] ?? "",
-      currency: infoJson['currency'] ?? "",
       price: infoJson['price'] ?? "",
-      price_gram_24k: infoJson['price_gram_24k'] ?? "",
-      price_gram_22k: infoJson['price_gram_22k'] ?? "",
-      price_gram_21k: infoJson['price_gram_21k'] ?? "",
-      price_gram_20k: infoJson['price_gram_20k'] ?? "",
-      price_gram_18k: infoJson['price_gram_18k'] ?? "",
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       "year": year,
-      "metal": metal,
-      "currency": currency,
       "price": price,
-      "price_gram_24k": price_gram_24k,
-      "price_gram_22k": price_gram_22k,
-      "price_gram_21k": price_gram_21k,
-      "price_gram_20k": price_gram_20k,
-      "price_gram_18k": price_gram_18k,
     };
   }
 }
 
 class SilverHisInfo {
-  String metal, currency;
-  double price,
-      price_gram_24k,
-      price_gram_22k,
-      price_gram_21k,
-      price_gram_20k,
-      price_gram_18k;
+  double price;
   int year;
-  SilverHisInfo(
-      {required this.year,
-      required this.metal,
-      required this.currency,
-      required this.price,
-      required this.price_gram_24k,
-      required this.price_gram_22k,
-      required this.price_gram_21k,
-      required this.price_gram_20k,
-      required this.price_gram_18k});
+  SilverHisInfo({
+    required this.year,
+    required this.price,
+  });
 
   factory SilverHisInfo.fromJson(Map<String, dynamic> infoJson) {
     return SilverHisInfo(
       year: infoJson['year'] ?? "",
-      metal: infoJson['metal'] ?? "",
-      currency: infoJson['currency'] ?? "",
       price: infoJson['price'] ?? "",
-      price_gram_24k: infoJson['price_gram_24k'] ?? "",
-      price_gram_22k: infoJson['price_gram_22k'] ?? "",
-      price_gram_21k: infoJson['price_gram_21k'] ?? "",
-      price_gram_20k: infoJson['price_gram_20k'] ?? "",
-      price_gram_18k: infoJson['price_gram_18k'] ?? "",
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       "year": year,
-      "metal": metal,
-      "currency": currency,
       "price": price,
-      "price_gram_24k": price_gram_24k,
-      "price_gram_22k": price_gram_22k,
-      "price_gram_21k": price_gram_21k,
-      "price_gram_20k": price_gram_20k,
-      "price_gram_18k": price_gram_18k,
     };
   }
 }

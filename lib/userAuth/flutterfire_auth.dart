0fa01a74c616b100db937bd935452c29bd6d5cdf
SimonOdischo <103313046+SimonOdischo@users.noreply.github.com>
2023-02-10 17:26:57 +0100

    firebase auth methods update

--- lib/userAuth/flutterfire_auth.dart
+++ lib/userAuth/flutterfire_auth.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:stock_x/services/provider/client.dart';
+import 'package:stock_x/services/provider/encryption.dart';
 
 /*
 Die Datei beinhaltet Methoden für die 
@@ -20,11 +23,80 @@ Future<bool> signIn(String email, String password) async {
   }
 }
 
-Future<bool> resetPassword(String mail) async {
+//Boolische Mehtode nimmt email & Passwort
+//liefert true || false
+Future<bool> register(String email, String password) async {
   try {
-    await FirebaseAuth.instance.sendPasswordResetEmail(email: mail.trim());
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String privatKey = Encryption.getSecretKey();
+
+    await prefs.setString("privatKey", privatKey);
+    String privatKeyHash = "";
+    privatKeyHash = Encryption.keyHash(privatKey);
+
+    List<String> itemList = [
+      "A.data",
+      "B.data",
+      "C.data",
+      "D.data",
+      "E.data",
+      "E.data",
+      "F.data",
+      "G.data",
+      "H.data",
+      "I.data",
+      "K.data",
+      "L.data",
+      "M.data",
+      "N.data"
+    ];
+    List<String> dataListe;
+
+    dataListe = await getList("Data for User", itemList);
+
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+
+//Doc für Regestreite User erstellen.
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+    await FirebaseFirestore.instance.collection('userData').doc(uid).set({
+      dataListe[0]: email,
+      dataListe[1]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[2]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[3]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[4]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[5]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[6]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[7]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[8]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[9]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[10]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[11]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[12]:
+          Encryption.dataCrypt("0", prefs.getString("privatKey").toString()),
+      dataListe[13]: privatKeyHash
+    });
+
     return true;
-  } on FirebaseException {
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+    }
+    return false;
+  } catch (e) {
     return false;
   }
 }
